c730871975377aee503e77a22d98073893b8f411
kmtekvn <70768772+kmtekvn@users.noreply.github.com>
2021-08-20 09:55:56 +0700

    update repo

--- 04. Slides/01. Intro Embedded/Labs/keilc-simulator/Src/main.c
+++ 04. Slides/01. Intro Embedded/Labs/keilc-simulator/Src/main.c
@@ -6,15 +6,22 @@ volatile uint32_t msTicks = 0;                              /* Variable to store
   
 extern uint32_t SystemCoreClock;
 
+
 void SysTick_Handler(void)  {                               /* SysTick interrupt Handler. */
   msTicks++;                                               
 }
 
 
 int main (void)  {
+  uint32_t a = 0x55;
+	uint32_t* ptr = 0;
+	
   uint32_t returnCode;
   
  SysTick_Config(SystemCoreClock / 1000);      /* Configure SysTick to generate an interrupt every millisecond */
  
+	ptr = &a;
+	*ptr = *ptr + 1;
+	
   while(1);
 }
