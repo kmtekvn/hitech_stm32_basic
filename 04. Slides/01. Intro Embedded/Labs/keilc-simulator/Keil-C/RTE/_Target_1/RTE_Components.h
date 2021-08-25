1a6f8e36dd488be1a337a36d528af7f33e448125
kmtekvn <70768772+kmtekvn@users.noreply.github.com>
2021-08-25 09:09:37 +0700

    update keil-c project

--- 04. Slides/01. Intro Embedded/Labs/keilc-simulator/Keil-C/RTE/_Target_1/RTE_Components.h
+++ 04. Slides/01. Intro Embedded/Labs/keilc-simulator/Keil-C/RTE/_Target_1/RTE_Components.h
@@ -1,6 +1,6 @@
 
 /*
- * Auto generated Run-Time-Environment Component Configuration File
+ * Auto generated Run-Time-Environment Configuration File
  *      *** Do not modify ! ***
  *
  * Project: 'basic' 
@@ -9,6 +9,13 @@
 
 #ifndef RTE_COMPONENTS_H
 #define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "ARMCM4.h"
+
 
 
 #endif /* RTE_COMPONENTS_H */
